54a86f6c57195b226aa519bb2b07a2607a385cef
Muchiri <developedbyrobbie@gmail.com>
2024-04-09 22:03:25 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <ModeToggle />
+    </>
+  );
+}
