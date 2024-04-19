b39fe80c27ae29cc05c7efb09b48ddb811635063
Muchiri <developedbyrobbie@gmail.com>
2024-04-19 21:42:39 +0300

    clerk webhook

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import { SignUp, currentUser } from "@clerk/nextjs";
 export default async function Page() {
   const user = await currentUser();
   await saveUser({
-    userId: user?.id!,
+    clerkId: user?.id!,
     username: user?.username!,
     avatar: user?.imageUrl!,
   })
